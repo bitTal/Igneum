dc2db42431767ca5951f9cbf98c19066aeaa9c43
bitTal <albafjez@gmail.com>
2016-03-31 20:54:03 +0200

    Backbone setups

---
+++ app/assets/javascripts/igneum.js
@@ -0,0 +1,13 @@
+window.Igneum = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Igneum.initialize();
+});
