5f404212524853008f7eab8973b0463c52cade31
bitTal <albafjez@gmail.com>
2016-05-09 00:06:56 +0200

    Set athorization to get into backoffice-edit

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -5,5 +5,16 @@ class BackofficeController < ApplicationController
 	def edit
 		@auth = request.env['omniauth.auth']
 		@users = User.all
+		@nickname = request.env['omniauth.auth']['info']['email'].split('@').first
+		@exists = @users.select {|user| user.user == @nickname } 
+
+		if @exists.length === 0
+			redirect_to action: "index"
+		else 
+			if @exists[0].user_type != 'S'
+				redirect_to action: "index"
+			end
+		end
+
 	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ conf = YAML.load_file("#{Dir.pwd}/config/confidencial.yml")
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, conf['client_id'], conf['client_secret'], {
-		scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online'
+		scope: 'email, profile', prompt: 'select_account', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', skip_jwt: true
 	}
 end
