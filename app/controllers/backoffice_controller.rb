979c23182fc45b966ca9c1db1a237595d3543bcc
bitTal <albafjez@gmail.com>
2016-05-17 00:53:08 +0200

    Add fires by select options

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -1,4 +1,4 @@
-
+require "#{Dir.pwd}/config/utils/index.rb"
 
 class BackofficeController < ApplicationController
 	def index
@@ -20,11 +20,23 @@ class BackofficeController < ApplicationController
 	end
 
 	def add_fire
-		if params['town']
+		@provs = Provincias.all
+		@prov = params['provincia'] ? params['provincia'] : '2'
+		@municipios = Municipios.where(id_provincia: @prov)
+
+		if params['town'] || params['municipios']
+			if params['town']
+				@town = params['town']	
+			elsif params['municipios']
+				@town =  Municipios.where(nombre: params['municipios'])[0]['nombre']
+			end
+
 			require('yaml')
 			conf = YAML.load_file("#{Dir.pwd}/config/confidencial.yml") 
-		    @town = params['town']
+			@town = normileze_string(@town)
 		    open("https://#{conf['cartodb_user']}.cartodb.com/api/v2/sql?q=INSERT INTO frs (town, date, country) VALUES ('#{@town}', now(), 'Spain')&api_key=#{conf['cartodb_api_key']}").read
-		end 
+		end
+
 	end
+
 end
