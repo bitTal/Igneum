2f9aa1889b34127e1b6a03bb178488c82651ce36
bitTal <albafjez@gmail.com>
2016-05-22 23:51:00 +0200

    Informing about new fire created

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -36,10 +36,11 @@ class BackofficeController < ApplicationController
 			redirect_to action: "index"
 			return
 		end
-				
+
 		@provs = Provincias.all
 		@prov = params['provincia'] ? params['provincia'] : '2'
 		@municipios = Municipios.where(id_provincia: @prov)
+		@town = flash[:var]
 	end
 
 	def create_fire
@@ -60,8 +61,10 @@ class BackofficeController < ApplicationController
 			conf = YAML.load_file("#{Dir.pwd}/config/confidencial.yml") 
 			@town = normileze_string(@town)
 		    open("https://#{conf['cartodb_user']}.cartodb.com/api/v2/sql?q=INSERT INTO frs (town, date, country) VALUES ('#{@town}', now(), 'Spain')&api_key=#{conf['cartodb_api_key']}").read
-		    redirect_to action: 'add_fire'
+			flash[:var] = @town
+			
 		end
+		redirect_to action: 'add_fire'
 	end
 
 	def close
