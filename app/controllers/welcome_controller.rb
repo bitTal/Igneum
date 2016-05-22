e2a779f22405a36e4119b689299b67d2db5117fc
bitTal <albafjez@gmail.com>
2016-05-22 19:46:03 +0200

    Default values in month & compare selects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,9 @@ class WelcomeController < ApplicationController
 			'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 		@months = auxMonths.map.with_index(1).to_a
 		@years = ['2016', '2015'];
+		@month = params['month1'] || Date.today.month
+		@year = params['year1'] || Date.today.year
+
 	end
 	
 	def compare
@@ -14,6 +17,10 @@ class WelcomeController < ApplicationController
 			'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 		@months = auxMonths.map.with_index(1).to_a
 		@years = ['2016', '2015'];
+		@month1 = params['month1'] || Date.today.month
+		@year1 = params['year1'] || Date.today.year
+		@month2 = params['month2'] || Date.today.month
+		@year2 = params['year2'] || Date.today.year
 	end
 
 end
