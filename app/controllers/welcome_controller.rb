3c10fe0a6d7060d46fc2d82ed6460aed5ce1ff7c
bitTal <albafjez@gmail.com>
2016-05-22 19:18:07 +0200

    Month select form connected

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,10 @@ class WelcomeController < ApplicationController
 	end
 
 	def month
+		auxMonths = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 
+			'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+		@months = auxMonths.map.with_index(1).to_a
+		@years = ['2016', '2015'];
 	end
 	
 	def compare
