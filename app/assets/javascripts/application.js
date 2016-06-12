667ff2573be9464db1de9985e054861413d80d34
bitTal <albafjez@gmail.com>
2016-06-12 19:16:52 +0200

    Assets js reference fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,5 @@
 //= require underscore
 //= require backbone
 //= require igneum
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
